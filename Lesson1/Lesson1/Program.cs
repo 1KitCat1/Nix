ccb3eebd6da14609079c7abbf7b9c34505545753
1KitCat1 <denischool119@gmail.com>
2021-05-19 19:47:06 +0300

    Lesson 1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+    public class Animal
+    {
+        public string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public string GetInfo()
+        {
+            return $"Animal {Name}";
+        }
+    }
+}
---
+++ Lesson1/Pr2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal = new Lesson1.Animal("dog");
+            Console.WriteLine(animal.GetInfo());
+            Console.Read();
+        }
+    }
+}
