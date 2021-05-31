925cd1f1cf639a10b44bce2d467f9bf8cedc321a
1KitCat1 <denischool119@gmail.com>
2021-05-31 17:57:17 +0300

    Module 1 (v3)

---
+++ Lesson2/Lesson2/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2
+{
+    abstract class Vehicle
+    {
+        public string ID { get; }
+        public bool isRented { get; protected set; }
+        public string Condition { get; protected set; }
+        private string type;
+        
+        /*public Vehicle {}*/
+
+
+    }
+}
--- Lesson2/Lesson2/VehicleCourier.cs
+++ Lesson2/Lesson2/VehicleCourier.cs
@@ -6,9 +6,10 @@ namespace Lesson2
 {
     class VehicleCourier : Courier
     {
+        public string VehicleID { get; protected set; }
         private bool isVehicleRented;
         private string vehicleStatus;
-
+        
         public VehicleCourier(string id, string name) : base(id, name)
         {
 
