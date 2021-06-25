555da3d7c3920384b6ec29a13221b76ae8eabb8b
1KitCat1 <denischool119@gmail.com>
2021-06-25 22:57:37 +0300

    Module2

---
+++ LessonLINQ/LessonLINQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LessonLINQ
+{
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Phone
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>()
+            {
+                new User {Name = "Adam", Age = 34},
+                new User {Name = "Mark", Age = 19}
+            };
+            var people = from user in users
+                         where user.Age > 20
+                         select new { Name = user.Name, Age = user.Age };
+            foreach(var person in people)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+    }
+}
---
+++ Module2DataBase/Module2DataBase/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+namespace Module2DataBase
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Добавление записи в БД
+            Console.WriteLine("Do you want to add new client? (YES|NO)");
+            var inputString = Console.ReadLine();
+            if(inputString == "YES" || inputString == "Yes")
+            {
+                string name, address;
+                int balance;
+                Console.WriteLine("Write NAME of the new client");
+                name = Console.ReadLine();
+                Console.WriteLine("Write ADDRESS of the new client");
+                address = Console.ReadLine();
+                Console.WriteLine("Write BALANCE of the new client");
+                balance = Convert.ToInt32( Console.ReadLine());
+                var newClient = new Client
+                {
+                    Name = name,
+                    Address = address,
+                    Balance = balance
+                };
+                using (var context = new DataBase())
+                {
+                    context.Clients.Add(newClient);
+                    context.SaveChanges();
+                }
+            }
+        
+            Console.WriteLine("Do you want to delete data?");
+            string input = Console.ReadLine();
+            if(input == "YES")
+            {
+                //Удаление записи в БД
+                using (var context = new DataBase())
+                {
+                    var delClient = context.Clients
+                   .Where(o => o.Name == "Ivan")
+                   .FirstOrDefault();
+                    context.Clients.Remove(delClient);
+                    context.SaveChanges();
+                }
+            }
+            
+            //Печать всех клентов
+            using (var context = new DataBase())
+            {
+                var clients = context.Clients;
+                foreach(var client in clients)
+                {
+                    Console.WriteLine($"Name: {client.Name} | Address:{client.Address}");
+                }
+            }
+            using(var context = new DataBase())
+            {
+                context.SaveChanges();
+            }
+            Console.ReadLine();
+        }
+    }
+}
